12c0ae5187387e8ccdddf2a6753934193f179f9b
Mike1ife <yitai77@gmail.com>
2023-12-19 19:23:33 +0800

    update

--- chatapp/models.py
+++ chatapp/models.py
@@ -43,37 +43,8 @@ class UserProfile(models.Model):
     # Add more fields as needed
 
     def __str__(self):
-
-        return self.question
-    
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    research_area = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的研究領域"
-    )
-    education = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的最高學歷"
-    )
-    key_skills = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的專業技能"
-    )
-    work_experiences = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的工作經驗"
-    )
-    relevant_coursework = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的相關課程"
-    )
-    extracurricular = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的課外活動"
-    )
-    language_skills = models.CharField(
-        max_length=100, blank=True, null=True, default="請輸入您的外語能力"
-    )
-
-    # Add more fields as needed
-
-    def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -84,4 +55,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
